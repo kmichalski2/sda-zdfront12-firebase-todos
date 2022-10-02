9eabd8e32ca3868e5d8884be23851d0612f66c6b
Kamil Michalski <kmichalski@outlook.com>
2022-10-02 14:42:49 +0200

    sign out

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { initAddForm } from "./add";
 import { initList } from "./list";
 import { firebaseConfig } from "./config";
@@ -16,8 +16,28 @@ const auth = getAuth(app);
 
 const tasksCollection = collection(db, "tasks");
 
-initList(db, tasksCollection);
-initAddForm(tasksCollection);
+const signOutButton = document.querySelector("#signOutButton");
+
+if (signOutButton) {
+  signOutButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    signOut(auth).then((result) => {
+      window.location.href = window.location.origin + "/login.html";
+    });
+  });
+}
+
+onAuthStateChanged(auth, (user) => {
+  // Zadanie 1
+
+  if (user) {
+    initList(db, tasksCollection);
+    initAddForm(tasksCollection);
+  } else {
+    console.log("Nie jestes zalogowany");
+  }
+});
 
 initRegisterForm(auth);
-initLoginForm();
+initLoginForm(auth);

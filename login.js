438f1dec1c6b15c7bdaddae9241dd19ff691f417
Kamil Michalski <kmichalski@outlook.com>
2022-10-02 14:23:08 +0200

    Login error handling

--- login.js
+++ login.js
@@ -1,11 +1,40 @@
-export const initLoginForm = () => {
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export const initLoginForm = (auth) => {
   const loginForm = document.querySelector("#loginForm");
 
   if (loginForm) {
-    console.log("Login form has been initialized");
-
     loginForm.addEventListener("submit", (event) => {
       event.preventDefault();
+
+      const formData = new FormData(loginForm);
+      const alerts = document.querySelector("#alerts");
+
+      signInWithEmailAndPassword(
+        auth,
+        formData.get("email"),
+        formData.get("password")
+      )
+        .then((result) => {
+          const message = "Login successful";
+          alerts.innerHTML = `<div class="alert alert-success">${message}</div>`;
+        })
+        .catch((error) => {
+          alerts.innerHTML = `<div class="alert alert-danger">${getErrorMessage(
+            error
+          )}</div>`;
+        });
     });
+  }
+};
+
+const getErrorMessage = (error) => {
+  switch (error.code) {
+    case "auth/wrong-password":
+      return "Provided password is wrong.";
+    case "auth/user-not-found":
+      return "User with given e-mail adress is not found";
+    default:
+      return error.message;
   }
 };

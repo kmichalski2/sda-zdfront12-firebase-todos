9f963f7b601032ccfebc84726ac5ec2dbf639370
Kamil Michalski <kmichalski@outlook.com>
2022-10-08 11:19:06 +0200

    Add Form Modal

--- add.js
+++ add.js
@@ -2,9 +2,12 @@ import { Timestamp, addDoc } from "firebase/firestore";
 
 export const initAddForm = (tasksCollection) => {
   const addForm = document.querySelector("#addForm");
+  const submitAddFormButton = document.querySelector("#submitAddFormButton");
+  // const addFormModal = document.querySelector("#addTaskModal");
+  const addFormModal = new bootstrap.Modal("#addTaskModal");
 
-  if (addForm) {
-    addForm.addEventListener("submit", (event) => {
+  if (addForm && submitAddFormButton && addFormModal) {
+    submitAddFormButton.addEventListener("click", (event) => {
       event.preventDefault();
 
       const formData = new FormData(addForm);
@@ -13,6 +16,7 @@ export const initAddForm = (tasksCollection) => {
       const deadlineTimestamp = Timestamp.fromDate(deadlineDate);
 
       const newTask = {
+        order: 999999,
         name: formData.get("name"),
         deadline: deadlineTimestamp,
         done: false,
@@ -21,6 +25,7 @@ export const initAddForm = (tasksCollection) => {
 
       addDoc(tasksCollection, newTask).then((data) => {
         console.log("Task has been added!");
+        addFormModal.hide();
       });
     });
   }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "./node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 import { initializeApp } from "firebase/app";
